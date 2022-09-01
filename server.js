d5fdd5ab3b87f88aaeed582fbeb31ec44f586ae0
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-09-01 21:40:34 +0530

    msg input done

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ io.on('connection', socket =>{
     
     socket.emit('message', 'Welcome to the chat-app');
 
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', ()=>{
+        io.emit('message','A user has left the chat');
+    });
+
+    socket.on('chatMessage', (msg)=>{
+       io.emit('message', msg)
+    })
+
 
 })
 
