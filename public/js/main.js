250faf71f84a53dc9e7de496ae409fc029dd3bce
Susmit Mukherjee <susmitmukherjee2001@gmail.com>
2022-09-07 01:31:21 +0530

    made compatible for hosting

--- public/js/main.js
+++ public/js/main.js
@@ -4,17 +4,22 @@ const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
 const currentUser = document.getElementById('currentUser');
 
+// Getting the username and room-name from the search query
 const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
   });
 
 const socket=io();
 
-
+//When user joins the room
 socket.emit('joinRoom',{username,room});
 
 socket.on('roomUsers',({room,users}) =>{
+
+    //Show room name
     outputRoomName(room);
+
+    //Show users in the room
     outputUsers(users);
 })
 
@@ -23,22 +28,27 @@ currentUser.innerHTML= `  ${username}`;
  
 // Display the welcome message
 socket.on('message', message =>{
+
+    //Output the message
     outputMessage(message);
 
     chatMessage.scrollTop = chatMessage.scrollHeight;
 });
+
 // Event after send btn is clicked
 chatForm.addEventListener('submit',(e) =>{
     e.preventDefault();
     const msg = e.target.elements.msg.value;
+
     //The msg content is emited
     socket.emit('chatMessage',msg);
 
     e.target.elements.msg.value='';
     e.target.elements.msg.focus();
 
-})
+});
 
+//Output the message
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
@@ -50,10 +60,12 @@ function outputMessage(message){
     document.querySelector('.chat-messages').appendChild(div);
 }
 
+//Show room name
 function outputRoomName(room){
     roomName.innerText=room;
 }
 
+//Show users in the room
 function outputUsers(users){
     userList.innerHTML = `
     ${users.map(user =>`<li>${user.username}</li>`).join('')
--- server.js
+++ server.js
@@ -8,19 +8,24 @@ const {userJoin,getCurrentUser,getRoomUsers,userLeave} = require('./utils/users'
 const app =express();
 const server= http.createServer(app);
 const io =socketio(server);
+const port = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname,'public')));
 
 //New Connection is Created
 io.on('connection', socket =>{
 
+//When user joins the room
     socket.on('joinRoom',({username,room}) =>{
-
+    //Storing the user info
        const user = userJoin(socket.id,username,room);
+       //logged user joins the selected room
        socket.join(user.room);
-    //When a new User joins
+
+    //Welcome msg to the user
         socket.emit('message', formatMessage('Admin','Welcome to the chat-app'));
-    //When a new User joins the room
+   
+        //When a new User joins the room
         socket.broadcast.to(user.room).emit('message', formatMessage('Admin',`${user.username} has joined the chat`));
 
         io.to(user.room).emit('roomUsers',{
@@ -31,18 +36,20 @@ io.on('connection', socket =>{
   
 
     });
-
-   
-   
-    // The received msg content is wrapped with formatMessage function and emited to 'message'
+   //When msg is send
     socket.on('chatMessage', msg =>{
         const user = getCurrentUser(socket.id);
+    // The received msg content is wrapped with formatMessage function and emited to 'message'
        io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
+
     //When a user leaves
+
     socket.on('disconnect', ()=>{
         const user = userLeave(socket.id);
+
         // Disconnecting message
+
         if(user){
             io.to(user.room).emit('message',formatMessage('Admin',`${user.username} has left the chat`));
 
@@ -57,7 +64,7 @@ io.on('connection', socket =>{
     });
 
 })
-
-server.listen(2000,()=>{
-    console.log("Server started on http://localhost:2000");
+//starting the server
+server.listen(port,()=>{
+    console.log(`running on port ${port}`);
 }) 
